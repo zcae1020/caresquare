201b570aaa35eb1ec109d0823bff6c1a23d119ac
zcae1020 <zcae1020@naver.com>
2023-02-10 22:36:41 +0900

    feat: 행동 출력하는 함수 구현

--- src/module/outputView.go
+++ src/module/outputView.go
@@ -24,7 +24,22 @@ func printDay(status [myconstant.MAX_STATUS_TYPE]int, actions []int) {
 }
 
 func printAction(action int) {
-
+	switch myconstant.Status(action) {
+	case myconstant.Study:
+		fmt.Println("프로그래밍 공부중...")
+	case myconstant.Eat:
+		fmt.Println("치킨 먹는중...")
+	case myconstant.Toliet:
+		fmt.Println("화장실입니다...")
+	case myconstant.Sleep:
+		fmt.Println("자는중...")
+	case myconstant.Play:
+		fmt.Println("유튜브 보는중...")
+	case myconstant.Wash:
+		fmt.Println("씻는중...")
+	case myconstant.Job:
+		fmt.Println("자소서 쓰는중...")
+	}
 }
 
 func printStatus(status [myconstant.MAX_STATUS_TYPE]int) {

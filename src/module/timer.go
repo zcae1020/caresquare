305726c9f5212c1a573079cb5217e1277a2bd6c4
zcae1020 <zcae1020@naver.com>
2023-02-10 21:43:43 +0900

    feat: 시간에 따른 상태변화 나타내는 함수 구현

--- src/module/timer.go
+++ src/module/timer.go
@@ -3,20 +3,23 @@ package module
 import "myconstant"
 
 const MINUTE_OF_HOUR = 60
+const STATE_CHANGE_PER_MINUTE = 5
 
 func getCurrentTime(time int) (int, int) {
 	return time / MINUTE_OF_HOUR, time % MINUTE_OF_HOUR
 }
 
-func changeStatusByTime() {
-
+func changeStatusByTime(status [myconstant.MAX_STATUS_TYPE]int) {
+	for i := 0; i < myconstant.MAX_STATUS_TYPE; i++ {
+		status[i] -= STATE_CHANGE_PER_MINUTE
+	}
 }
 
 func nextTime(status [myconstant.MAX_STATUS_TYPE]int, time int) int {
 	for i := 0; i <= myconstant.TIME_INTERVAL; i += 10 {
 		printTime(getCurrentTime(time + i))
-		changeStatusByTime()
-		printStatus(status)
+		changeStatusByTime(status)
 	}
+	printStatus(status)
 	return time + myconstant.TIME_INTERVAL
 }

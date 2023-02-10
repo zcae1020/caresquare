a73fbc4a3a1efcd2b6be367a8444c25219f911ca
zcae1020 <zcae1020@naver.com>
2023-02-10 19:59:08 +0900

    feat: initStatus 구현

--- src/module/init.go
+++ src/module/init.go
@@ -1,7 +1,13 @@
 package module
 
-func InitStatus() {
+import (
+	"myconstant"
+)
 
+func InitStatus(status []int) {
+	for i := 0; i < myconstant.MAX_STATUS_TYPE; i++ {
+		status[i] = 100
+	}
 }
 
 func StartDay() {

90f25dcd5d97033d632a17d604642ae542f996f3
zcae1020 <zcae1020@naver.com>
2023-02-11 11:54:35 +0900

    feat: 현재 상태 진단 함수 구현

--- src/module/diagnosis.go
+++ src/module/diagnosis.go
@@ -1,1 +1,15 @@
 package module
+
+import "myconstant"
+
+const THRESHOLD = 20
+
+func diagnose(status [myconstant.MAX_STATUS_TYPE]int) (leakList []int) {
+	for i := 0; i < myconstant.MAX_STATUS_TYPE; i++ {
+		if status[i] < THRESHOLD {
+			leakList = append(leakList, i)
+		}
+	}
+
+	return
+}
